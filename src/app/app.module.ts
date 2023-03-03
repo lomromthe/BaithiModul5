7e6f140e8a6dcc914efc829dc245f9479dcbe248
lomromthe <114889821+lomromthe@users.noreply.github.com>
2023-03-03 10:52:45 +0700

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ShowComponent } from './tour/show/show.component';
+import { EditComponent } from './tour/edit/edit.component';
+import { DeleteComponent } from './tour/delete/delete.component';
+import { CreateComponent } from './tour/create/create.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShowComponent,
+    EditComponent,
+    DeleteComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
